798a90802a7aabdbd35de504f8e88d02ff1fb940
Xynon2210 <53427626+Xynon2210@users.noreply.github.com>
2024-11-15 20:33:14 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function scrollToAbout() {
+    document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
+}
+/*
+function scrollToSkills() {
+    document.getElementById("skills").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToResume() {
+    document.getElementById("resume").scrollIntoView({ behavior: 'smooth' });
+}
+*/
+
